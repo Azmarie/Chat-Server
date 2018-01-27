0dc154b391544d70c33d379cb858097bc11eddd0
ywa194 <ywa194@sfu.ca>
2018-01-27 15:39:32 -0800

    add initials for chat

--- client/app.js
+++ client/app.js
@@ -1,7 +1,13 @@
 var socket = io();
 
 $('form').submit(function () {
-  var text = $('#message').val();
+  var name = $('#initials').val();
+  $('#initials').val('');
+
+  var message = $('#message').val();
+  // string interpolation:
+  var text = `${name} says: ${message}`
+  // var text = name + " says: " + message;
   socket.emit('message', text);
   $('#message').val('');
   return false;
